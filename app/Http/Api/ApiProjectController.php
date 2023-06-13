e76e0696343be199f69118383be4fee2394c9273
nicolozibra1 <nico.zibra@gmail.com>
2023-06-13 15:16:20 +0200

    api project controller

---
+++ app/Http/Api/ApiProjectController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ApiProjectController extends Controller
+{
+    public function index() {
+        $projects = Project::all();
+        return response()->json([
+            'success'=> true,
+            'results'=> $projects
+        ]);
+    }
+
+    public function show($slug){
+        $project = Project::with('type')->where('slug', $slug)->first();
+
+        if($project){
+            return response()->json([
+                'success'=> true,
+                'results'=> $project
+            ]);
+        }
+        else {
+            return response()->json([
+                'success'=> false,
+                'results'=> 'Project not found!'
+            ]);
+        }
+    }
+}
